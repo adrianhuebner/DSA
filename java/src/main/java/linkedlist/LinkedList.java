20731ef9a9fba0eb62d552294a735919b2d68497
adrianhuebner <adrianh92@gmail.com>
2020-03-04 22:49:30 -0800

    New methods added to linked list and more tests added to linked list tests

--- java/src/main/java/linkedlist/LinkedList.java
+++ java/src/main/java/linkedlist/LinkedList.java
@@ -1,5 +1,7 @@
 package linkedlist;
 
+import java.util.StringJoiner;
+
 // got the set up for this linked list from geeks for geeks
 
 public class LinkedList{
@@ -8,36 +10,97 @@ public class LinkedList{
     static class Node {
         int data;
         Node next;
-        public Node(int data, linkedlist.LinkedList.Node next) {
+
+        Node(int value){
+            this(value, null);
+        }
+
+        Node(int data, Node next) {
             this.data = data;
             this.next = next;
         }
     }
 
     public void insert(int newValue){
-        Node node = new Node();
-        node.value = newValue
-        if(head == null){
-            head == node;
-        } else {
+        Node node = new Node(newValue);
+        node.data = newValue;
+        if (head != null) {
             node.next = head;
-            head = node;
         }
+        head = node;
     }
 
     public boolean includes(int searchValue){
-        int currentSpot = this.head;
+        Node currentSpot = this.head;
         while(currentSpot != null){
             if(currentSpot.data == searchValue){
                 return true;
             } else{
                 currentSpot = currentSpot.next;
             }
-            return false;
         }
+        return false;
     }
 
     public String toString(){
+        Node current = head;
+        StringJoiner string = new StringJoiner(" ");
+        string.add("String Linked List: ");
 
+        while(current != null){
+            string.add("" + current.data);
+            current = current.next;
+        }
+        return string.toString();
     }
+
+    public void append(int newValue){
+        Node node = new Node(newValue);
+        if(this.head == null){
+            this.head = node;
+        } else {
+            Node endNode = this.head;
+            while(endNode.next != null){
+                endNode = endNode.next;
+            }
+            endNode.next = node;
+        }
+    }
+
+    // thanks to geekviewpoint.com for an example of how to write this code that I was inspired by
+    public void insertBefore(int value, int newValue){
+        Node newNode = new Node(newValue);
+        if(head.data == value){
+            insert(newValue);
+        } else{
+            Node previous = head;
+            Node current = head.next;
+            while(current != null && current.data != value){
+                previous = previous.next;
+                current = current.next;
+            }
+            if(current == null){
+                System.out.println("Original value is not in the linked list");
+            } else {
+                newNode.next = current;
+                previous.next = newNode;
+            }
+        }
+    }
+
+    // thanks to geekviewpoint.com and geeksforgeeks.com for having code that I used as an inspiration
+    public void insertAfter(int value, int newValue){
+        Node newNode = new Node(newValue);
+        Node current = head;
+        while(current != null && current.data != value){
+            current = current.next;
+        }
+        if(current == null){
+            System.out.println("Original value is not in the linked list");
+        } else {
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+    }
+
 }
--- java/src/test/java/linkedlist/LinkedListTest.java
+++ java/src/test/java/linkedlist/LinkedListTest.java
@@ -5,22 +5,22 @@ import org.junit.Before;
 
 import static org.junit.Assert.*;
 
-public class LinkedTest{
+public class LinkedListTest{
 
     LinkedList newLink = new LinkedList();
     @Before
-        public void settingUp(){
-            newLink.insert(10);
-            newLink.insert(20);
-            newLink.insert(30);
-        }
+    public void settingUp(){
+        newLink.insert(10);
+        newLink.append(20);
+        newLink.append(30);
+    }
 
     @Test
     public void createNewLinkedList(){
         assertEquals(
                 "The head of linked list should be 10",
                 10,
-                newList.head.data
+                newLink.head.data
         );
     }
 
@@ -28,9 +28,9 @@ public class LinkedTest{
     @Test
     public void returnTrueIfExists(){
         assertEquals(
-                "This should be true that the linked list includs 20",
+                "This should be true that the linked list includes 20",
                 true,
-                newLink.contains(20)
+                newLink.includes(20)
         );
     }
 
@@ -39,7 +39,66 @@ public class LinkedTest{
         assertEquals(
                 "This should return false because the linked list doesn't include 40",
                 false,
-                newLink.contains(40)
+                newLink.includes(40)
+        );
+    }
+
+    @Test
+    public void convertsToString(){
+        assertEquals(
+                "The string should look like String Linked List: 30 20 10",
+                "String Linked List:  10 20 30",
+                newLink.toString()
+        );
+    }
+
+    @Test
+    public void canAddBeforeMiddle(){
+        newLink.insertBefore(20,15);
+        assertEquals(
+                "15 should be added to the middle of the linked list",
+                true,
+                newLink.includes(15)
+        );
+    }
+
+    @Test
+    public void canAddBeforeLast(){
+        newLink.insertBefore(30,25);
+        assertEquals(
+                "25 should be added to the linked list",
+                true,
+                newLink.includes(25)
+        );
+    }
+
+    @Test
+    public void canAddBeforeFront(){
+        newLink.insertBefore(10, 5);
+        assertEquals(
+                "5 should be included on the linked list",
+                true,
+                newLink.includes(5)
+        );
+    }
+
+    @Test
+    public void canAddAfterMiddleNode(){
+        newLink.insertAfter(20,25);
+        assertEquals(
+                "should be included on the linked list",
+                true,
+                newLink.includes(25)
+        );
+    }
+
+    @Test
+    public void canAddAtTheEnd(){
+        newLink.insertAfter(30,35);
+        assertEquals(
+                "should be included on the linked list",
+                true,
+                newLink.includes(35)
         );
     }
 }
