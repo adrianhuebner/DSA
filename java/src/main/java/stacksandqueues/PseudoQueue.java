0842116f3612aef7b492ea9e323dfb0b9c797a94
adrianhuebner <adrianh92@gmail.com>
2020-03-28 17:02:09 -0700

    PseudoQueue code written

--- java/src/main/java/stacksandqueues/PseudoQueue.java
+++ java/src/main/java/stacksandqueues/PseudoQueue.java
@@ -2,13 +2,25 @@ package stacksandqueues;
 
 public class PseudoQueue {
 
-    Stack stackOne = new Stack<>();
-    Stack stackTwo = new Stack<>();
+    Stack<Integer> stackOne = new Stack<>();
+    Stack<Integer> stackTwo = new Stack<>();
 
     public PseudoQueue(Stack stackOne, Stack stackTwo) {
         this.stackOne = stackOne;
         this.stackTwo = stackTwo;
     }
 
-    public void enqueue()
+    public void enqueue(int N){
+        while(!stackTwo.isEmpty()){
+            stackOne.push(stackTwo.pop());
+        }
+        stackOne.push(N);
+    }
+
+    public int dequeue(){
+        while(!stackOne.isEmpty()){
+            stackTwo.push(stackOne.pop());
+        }
+        return stackTwo.pop();
+    }
 }
