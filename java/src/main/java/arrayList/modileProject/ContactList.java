c26d473304a13a50c17491096a1a30a355546d2f
adrianhuebner <adrianh92@gmail.com>
2020-04-05 18:09:59 -0700

    Got code into ContactList, started creating methods in MobilePhone

--- java/src/main/java/arrayList/modileProject/ContactList.java
+++ java/src/main/java/arrayList/modileProject/ContactList.java
@@ -1,4 +1,32 @@
 package arrayList.modileProject;
 
 public class ContactList {
+    private String contactName;
+    private int phoneNumber;
+
+    public ContactList(String contactName, int phoneNumber) {
+        this.contactName = contactName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "" + contactName + ", Phone Number: " + phoneNumber;
+    }
 }
--- java/src/main/java/arrayList/modileProject/MobilePhone.java
+++ java/src/main/java/arrayList/modileProject/MobilePhone.java
@@ -1,4 +1,46 @@
 package arrayList.modileProject;
 
+import java.util.ArrayList;
+
 public class MobilePhone {
+    private ArrayList<ContactList> contactList;
+
+    public boolean addNewContact(ContactList contact){
+        String contactName = contact.getContactName();
+
+        if(findContact(contact) >= 0){
+            System.out.println(contactName + " already exists in your contact list");
+            return false;
+        } else {
+            contactList.add(contact);
+            System.out.println(contactName + " has been added to your contacts");
+            return true;
+        }
+    }
+
+    public boolean updateContact(ContactList oldContactInfo, ContactList newContactInfo){
+        int foundPosition = findContact(oldContactInfo);
+        if(foundPosition < 0){
+            System.out.println(oldContactInfo.getContactName() + ", was not found in your contact list");
+            return false;
+        }
+        this.contactList.set(foundPosition, newContactInfo);
+        System.out.println(oldContactInfo.getContactName() + " was replaced with " + newContactInfo.getContactName());
+        return true;
+    }
+
+    private int findContact(ContactList contact){
+        return this.contactList.indexOf(contact);
+    }
+
+    private int findContact(String contactName){
+        for(int i = 0; i < this.contactList.size(); i++){
+            ContactList contact = this.contactList.get(i);
+            if(contact.getContactName().equals(contactName)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
