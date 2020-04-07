d548923a948458e97a3a09810a968704be7bfbeb
adrianhuebner <adrianh92@gmail.com>
2020-04-06 20:04:29 -0700

    Quick Union Class code has been added

--- java/src/main/java/random/QuickFindUnion.java
+++ java/src/main/java/random/QuickFindUnion.java
@@ -1,5 +1,29 @@
 package random;
 
 public class QuickFindUnion {
-    
+    private int[] id;
+
+    public QuickFindUnion(int N){
+        id = new int[N];
+        for(int i = 0; i < N; i++){
+            id[i] = i;
+        }
+    }
+
+    private int root(int i){
+        while(i != id[i]){
+            i = id[i];
+        }
+        return i;
+    }
+
+    public boolean connected(int a, int b){
+        return root(a) == root(b);
+    }
+
+    public void union(int a, int b){
+        int i = root(a);
+        int j = root(b);
+        id[i] = j;
+    }
 }
