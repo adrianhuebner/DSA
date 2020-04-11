98815048825b497a3327f4f375f5d3e092b3c3b9
adrianhuebner <adrianh92@gmail.com>
2020-04-10 17:07:46 -0700

    Bank code finished

--- java/src/main/java/arrayList/bankingAutobox/Branch.java
+++ java/src/main/java/arrayList/bankingAutobox/Branch.java
@@ -6,41 +6,46 @@ public class Branch {
     private String name;
     private ArrayList<Customer> customers;
 
-    private Branch(String name){
+    public Branch(String name) {
         this.name = name;
         this.customers = new ArrayList<Customer>();
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public boolean newCustomer(String customerName, double initialAmount){
-        if(findCustomer(customerName) == null){
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String customerName, double initialAmount) {
+        if(findCustomer(customerName) == null) {
             this.customers.add(new Customer(customerName, initialAmount));
             return true;
         }
-        System.out.println(customerName + " already exists");
+
         return false;
     }
 
-    public boolean addCustomerTransaction(String customerName, double amount){
-       Customer existingCustomer = findCustomer(customerName);
-       if(existingCustomer != null){
-           existingCustomer.addTransaction(amount);
-           return true;
-       }
-        System.out.println(customerName + " was not found");
-       return false;
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer existingCustomer = findCustomer(customerName);
+        if(existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+
+        return false;
     }
 
-    private Customer findCustomer(String customerName){
-        for(int i = 0; i < this.customers.size(); i++){
+    private Customer findCustomer(String customerName) {
+        for(int i=0; i<this.customers.size(); i++) {
             Customer checkedCustomer = this.customers.get(i);
-            if(checkedCustomer.getName().equals(customerName)){
+            if(checkedCustomer.getName().equals(customerName)) {
                 return checkedCustomer;
             }
         }
+
         return null;
     }
 }
