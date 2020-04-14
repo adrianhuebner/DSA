b2d493a17b5e49484527625d5c7023d54b3e49ec
adrianhuebner <adrianh92@gmail.com>
2020-04-13 20:37:18 -0700

    Main code finished

--- java/src/main/java/arrayList/bankingAutobox/Main.java
+++ java/src/main/java/arrayList/bankingAutobox/Main.java
@@ -2,6 +2,23 @@ package arrayList.bankingAutobox;
 
 public class Main {
     public static void main(String[] args) {
+        Bank bank = new Bank("National Australia Bank");
 
+        bank.addBranch("Adelaide");
+        bank.addCustomer("Adelaide", "Olive", 10000.00);
+        bank.addCustomer("Adelaide", "Alley", 1000.00);
+        bank.addCustomer("Adelaide", "Maggie", 10000.00);
+
+        bank.addBranch("Sydney");
+        bank.addCustomer("Sydney", "Pip", 10000.00);
+
+        bank.addCustomerTransaction("Adelaide", "Olive", 1000.50);
+        bank.addCustomerTransaction("Adelaide", "Olive", 10000.50);
+        bank.addCustomerTransaction("Adelaide", "Maggie", 1000.50);
+        bank.addCustomerTransaction("Adelaide", "Maggie", 10000.50);
+
+        bank.addCustomerTransaction("Sydney", "Pip", 100000.75);
+
+        bank.listAllCustomers("Adelaide", true);
     }
 }
