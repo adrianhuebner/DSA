a0e2a046e657f88ad41ea346dbebb7a7cde809f8
adrianhuebner <adrianh92@gmail.com>
2020-05-02 20:35:13 -0700

    arrayList added to compare difference between array and linked list

--- java/src/main/java/linkedlist/onlineLearning/Main.java
+++ java/src/main/java/linkedlist/onlineLearning/Main.java
@@ -1,5 +1,7 @@
 package linkedlist.onlineLearning;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer("Olive", 1000.00);
@@ -11,6 +13,24 @@ public class Main {
                 // customer is the first class and anotherCustomer is the second one that got created which is actually pointing to the first one
         anotherCustomer.setBalance(10000.00);
         System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for(int i = 0; i < intList.size(); i++){
+            System.out.println(i + ": " + intList.get(i));
+        }
+        intList.add(1, 2);
+
+        for(int i = 0; i < intList.size(); i++){
+            System.out.println(i + ": " + intList.get(i));
+        }
+        // when you do the above code to an arrayList, what you are doing is moving down every element down after the index that you chose
+        // to do this when you have a lot of data that in that arrayList it is going to really slow down your program
+        // when you have to remove an item from an arrayList all the items have to move up in their index, which can slow down computer processing time
     }
     // Additional Notes:
         // This shows how memory is used and allocated within Java
