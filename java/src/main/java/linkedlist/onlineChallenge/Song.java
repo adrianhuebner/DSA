6de092ac01777ffa1e6862df26544b8ff35bf920
adrianhuebner <adrianh92@gmail.com>
2020-05-03 14:55:40 -0700

    File path set up for onlineChallenge and code for Song class started

--- java/src/main/java/linkedlist/onlineChallenge/Song.java
+++ java/src/main/java/linkedlist/onlineChallenge/Song.java
@@ -1,4 +1,19 @@
 package linkedlist.onlineChallenge;
 
 public class Song {
+    private String songName;
+    private String songDuration;
+
+    public Song(String songName, String songDuration) {
+        this.songName = songName;
+        this.songDuration = songDuration;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public String getSongDuration() {
+        return songDuration;
+    }
 }
